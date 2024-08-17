51c215f79e61321aa74bc9c531230ebb48758e95
priya <priya.saini.31992@gmail.com>
2024-08-17 03:03:11 +0100

    06082024

---
+++ src/ex-17082024/lab022.py
@@ -0,0 +1,20 @@
+#variable_name=variable_value
+#python is dynamic types language--> Literal value are determine the datatype.
+
+age=65
+#binary - base system-->2
+Binary_number=ob100
+
+hex--> base 16
+
+pi=3.14
+
+is_pramod_married = false #bool
+
+#string,str
+name="pramod"
+
+#complex
+
+complex_number=1+7j
+
