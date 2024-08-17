004c3678f4e96d3a837ac6837634d8d3b5d94a34
priya <priya.saini.31992@gmail.com>
2024-08-17 13:45:32 +0100

    17082024

--- src/ex-17082024/lab022.py
+++ src/ex-17082024/lab022.py
@@ -1,15 +1,32 @@
+#literals
 #variable_name=variable_value
 #python is dynamic types language--> Literal value are determine the datatype.
 
 age=65
+#decimal system 10
 #binary - base system-->2
-Binary_number=0b100
+Binary_number=0b1010
+
+#octal --> base8
+o= 0o130  # 88
 
 #hex--> base 16
 
 pi=3.14
 
-is_pramod_married = false #bool
+#is_pramod_married = false #bool
+
+#string, str
+name="parmod"
+
+#complex
+complex_number =+5j
+
+#list
+My_list=["pramod","amit","lucky"]
+
+#tuple
+my_tuple=("bread","apple","milk")
 
 #string,str
 name="pramod"
---
+++ src/ex-17082024/lab024.py
@@ -0,0 +1,19 @@
+#Escape sequence
+print("hello world")
+print("hello\n world") #\n-->new line
+print("hello\t world") #\t-->tab line
+print("hello \bworld") #\n-->back space
+#dir='c:pramod\n:txt'
+# where this r concept will be used?
+#Automation API, Web Automation,file_path=r concept
+dir=r'c:pramod\n:txt'
+dir2=r"c:pramod\n:txt"
+print(dir)
+print(dir2)
+
+#Escape Sequence
+#name='pramod'utta'
+nam2='pramod\'utta'
+name="pramod'utta"
+print(name)
+print(nam2)
