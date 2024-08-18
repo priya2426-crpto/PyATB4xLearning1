aae96808e544f173d236725ea7f9f2716b425144
priya <priya.saini.31992@gmail.com>
2024-08-18 14:56:30 +0100

    17082024

--- src/assignment001/lab010.py
+++ src/assignment001/lab010.py
@@ -6,5 +6,7 @@ print(name[0])
 print(name[1])
 print(name[2])
 print(name[3])
+
+# like this
 for character in name:
     print(character)
---
+++ src/ex-17082024/lab030.py
@@ -0,0 +1,15 @@
+#Ternary operator
+
+#if condition then do this
+#   else do that
+
+#if my_age>13 then i can watch reels
+#   else i can't watch
+
+user_age=int(input("enter your age"))
+#print("I will go GOA" if user_age>18 else "can't go,saty home")
+
+if user_age>18:
+    print("I will go GOA")
+else:
+    print("can't go,stay home")
