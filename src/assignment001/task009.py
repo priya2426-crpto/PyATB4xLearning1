49e8cf05efeeecbe5074e8d29a77e71fc646bb61
priya <priya.saini.31992@gmail.com>
2024-08-22 15:24:46 +0100

    20082024

--- src/assignment001/task009.py
+++ src/assignment001/task009.py
@@ -1,11 +1,11 @@
 """write a program that prints number from1 to 100, however, for the multiples of 3,
 print"fizz" instead of number, and for the multile of 5 print"buzz, the number multiple of 3 and 5 is fizzbuzz """
-for i in  range(1,101):
+for i in  range(101):
     if i%3==0 and i%5==0:
         print("fizzbuzz")
-    else i %3==0:
+    elif i%3==0:
         print("fizz")
-    else i %5==0:
+    elif i%5==0:
         print("buzz")
     else:
         print(i)
